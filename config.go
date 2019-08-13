d315337e8f7769f364112f4391c12c8e124f991d
Wyatt Miller <wjmiller2016@gmail.com>
2019-08-13 10:48:47 -0400

    yaml stuff

---
+++ config.go
@@ -0,0 +1,12 @@
+package main
+
+type Configuration struct {
+	Username string
+	Password string
+	Port     int
+	Hosts    []string
+}
+
+func initializeConfig() {
+
+}
---
+++ ssh.go
@@ -0,0 +1,6 @@
+package main
+
+type sshConnection struct {
+	Username string
+	Password []string
+}
